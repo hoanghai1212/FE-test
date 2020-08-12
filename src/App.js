c639edde08ed5b1ce2b5b6cfd5882967cfe3f7bf
hoanghai1212 <hoanghai129612@gmail.com>
2020-08-12 11:00:57 +0700

    now can search pokemon by name or number

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import SearchArea from "./component/SearchArea/SearchArea";
+import CardContainer from "./component/CardContainer/CardContainer";
+import classes from "./App.module.scss";
 
 function App() {
+  console.log("[App]");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.App}>
+      <div className={classes.App__Title}>
+        <h3>Pokédex</h3>
+      </div>
+      <SearchArea />
+      <CardContainer />
     </div>
   );
 }
---
+++ src/redux/actions/PokemonActions.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { FETCH_POKEMON, LOAD_MORE, SEARCH } from "./types/PokemonTypes";
+
+export const fetchPokemon = () => (dispatch) => {
+  axios
+    .get(
+      "https://thingproxy.freeboard.io/fetch/https://www.pokemon.com/us/api/pokedex/kalos",
+      { headers: { Accept: "application/json" } }
+    )
+    .then((res) => {
+      dispatch({
+        type: FETCH_POKEMON,
+        payload: {
+          data: res.data,
+          loading: false,
+        },
+      });
+    })
+    .catch((error) => console.log(error));
+};
+
+export const getPokemonList = (pageNumber) => ({
+  type: LOAD_MORE,
+  payload: pageNumber,
+});
+
+export const searchPokemonList = (keyword) => ({
+  type: SEARCH,
+  payload: keyword,
+});
