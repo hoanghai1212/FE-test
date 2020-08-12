f10670c3459f6c30e9f936439490c1973567b300
hoanghai1212 <hoanghai129612@gmail.com>
2020-08-12 11:32:13 +0700

    url

--- src/redux/actions/PokemonActions.js
+++ src/redux/actions/PokemonActions.js
@@ -3,10 +3,12 @@ import { FETCH_POKEMON, LOAD_MORE, SEARCH } from "./types/PokemonTypes";
 
 export const fetchPokemon = () => (dispatch) => {
   axios
-    .get(
-      "https://www.pokemon.com/us/api/pokedex/kalos",
-      { headers: { Accept: "application/json" } }
-    )
+    .get("https://www.pokemon.com/us/api/pokedex/kalos", {
+      headers: {
+        Accept: "application/json",
+        "Access-control-allow-origin": "116.108.218.105",
+      },
+    })
     .then((res) => {
       dispatch({
         type: FETCH_POKEMON,
